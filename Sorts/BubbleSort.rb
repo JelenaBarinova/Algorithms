f832d4553f41b28d7beb1324874594303ca12c59
JelenaBarinova <fiodorova.jelena@gmail.com>
2014-11-25 10:47:38 -0800

    Tabs to spaces

--- Sorts/BubbleSort.rb
+++ Sorts/BubbleSort.rb
@@ -2,19 +2,19 @@
 require_relative 'SortUtils'
 
 def BubbleSort (array)
-	if array.empty? then return nil end
-	if array.size == 1 then return array
-	else
-		i = 0
-		for i in 0..array.size() - 1
-			for j in 1..array.size() - 1 - i
-				if array[j - 1] > array[j] then
-					exch(array, j-1, j)
-				end
-				j += 1
-			end
-			i += 1
-		end
-		return array
-	end
+    if array.empty? then return nil end
+    if array.size == 1 then return array
+    else
+        i = 0
+        for i in 0..array.size() - 1
+            for j in 1..array.size() - 1 - i
+                if array[j - 1] > array[j] then
+                    exch(array, j-1, j)
+                end
+                j += 1
+            end
+            i += 1
+        end
+        return array
+    end
 end
